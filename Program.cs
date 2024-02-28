390d29d883afe8e1f0c4303f596199c034288a7d
nfalak-9 <nfalak@usf.edu>
2024-02-28 14:34:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Bubble_Sort
+{
+    class MySort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 62, 5, 16, 4, 10 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <=arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted: ");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
